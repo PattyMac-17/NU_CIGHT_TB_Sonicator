f19b44f109d6124276cbbb436272ffa24f3d5f5a
Patrick McLean <patrickmclean2027@u.northwestern.edu>
2026-06-02 00:13:49 -0500

    git test

--- Code/Python/continuous_polling_test.py
+++ Code/Python/continuous_polling_test.py
@@ -5,23 +5,25 @@ count = 0
 avgError = 0.0
 targetForce = 30.0
 
-mark = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.2)
-teensy = serial.Serial('/dev/ttyACM1', 115200, timeout=0.1)
+mark = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.2)
+teensy = serial.Serial("/dev/ttyACM1", 115200, timeout=0.1)
 time.sleep(0.1)
 while True:
-    mark.write(b'?\r')
+    mark.write(b"?\r")
     response = mark.readline()
-    response = response.decode(errors='ignore').strip()
+    response = response.decode(errors="ignore").strip()
     try:
         force = float(response)
     except:
         continue
-    teensy.write((response + '\n').encode())
-    
+    teensy.write((response + "\n").encode())
+
     error = force - targetForce
     count += 1
     avgError += (error - avgError) / count
     print(f"{force:.2f} N | avgError: {avgError:.4f} | count: {count}")
-    
-    time.sleep(1/30)
+
+    time.sleep(1 / 30)
 mark.close()
+
+##testing git
